b0d266e58a93234eefcc751b0dec2300d51fec61
anjkk01 <anjkk01@gmail.com>
2024-07-02 22:45:01 +0530

    new

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-
-const Home = () => {
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+        <div className="home-content">
+            <header className='home-header'>
+                <HeaderBox
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.name || 'Guest'}
+                subtext = "Access and manage your account and transactions efficiently."
+                />
+            <TotalBalanceBox
+              accounts={[]}
+              totalBanks={1}
+              totalCurrentBalance={1250.35}
+            />
+            </header>
+             Recent Transactions
+        </div>
+        <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 123.50},{currentBalance: 500}]} />
+    </section>
   )
 }
 
