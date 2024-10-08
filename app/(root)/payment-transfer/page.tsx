bb4b21c82d7a730b198ca7723750355eeec5ce7f
anjkk01 <121030480+anjkk01@users.noreply.github.com>
2024-10-09 03:55:13 +0530

    Add files via upload

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,6 +1,6 @@
 import HeaderBox from '@/components/HeaderBox'
 import PaymentTransferForm from '@/components/PaymentTransferForm'
-import { getAccounts } from '@/lib/actions/banks.actions';
+import { getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
